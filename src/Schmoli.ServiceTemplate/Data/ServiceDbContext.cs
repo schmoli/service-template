83c0b538b4e5b24aa124f3e17024f91c69ea0306
schmoli <schmoli@gmail.com>
2020-10-30 12:47:04 -0700

    Addign auto-utc conversion trick I learned.

--- src/Schmoli.ServiceTemplate/Data/ServiceDbContext.cs
+++ src/Schmoli.ServiceTemplate/Data/ServiceDbContext.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Options;
 using Schmoli.ServiceTemplate.Data.Configurations;
 using Schmoli.ServiceTemplate.Models;
 using Schmoli.Services.Core.Data.Postgres;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Schmoli.ServiceTemplate.Data
 {
@@ -25,6 +26,8 @@ namespace Schmoli.ServiceTemplate.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            ReadDatesAsUtc(builder);
+
             builder
                 .ApplyConfiguration(new PrimaryItemEntityTypeConfiguration())
                 .ApplyConfiguration(new SecondaryItemEntityTypeConfiguration())
@@ -36,6 +39,27 @@ namespace Schmoli.ServiceTemplate.Data
             optionsBuilder
                 .UseNpgsql(_settings.ConnectionString,
                 x => x.MigrationsHistoryTable("MigrationHistory", "migrations"));
+        }
+
+        /// <summary>
+        /// Set all read datetimes to UTC
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected void ReadDatesAsUtc(ModelBuilder modelBuilder)
+        {
+            var converter = new ValueConverter<DateTime, DateTime>(
+                x => x,
+                x => DateTime.SpecifyKind(x, DateTimeKind.Utc));
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                foreach (var property in entityType.GetProperties())
+                {
+                    if (property.ClrType == typeof(DateTime) || property.ClrType == typeof(DateTime?))
+                    {
+                        property.SetValueConverter(converter);
+                    }
+                }
+            }
         }
     }
 }
